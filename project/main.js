203591a0208c085030dac78506da900e2f8eca90
Yago.barros96 <yago.izidio96@gmail.com>
2021-06-27 18:31:52 -0300

    Novos controles e style na pagina

--- project/main.js
+++ project/main.js
@@ -32,3 +32,33 @@ window.addEventListener('scroll', function () {
         header.classList.remove('scroll')
     }
 })
+
+/* Testimonials carousel slider swiper*/
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+/* Scrollreveal: Mostrar os elementos quando der scroll na página*/
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .text, #home .image,
+    #about .text, #about .image,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+
+    
+    `, { interval: 100 })
